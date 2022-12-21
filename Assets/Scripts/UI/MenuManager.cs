096bd3f88916ec8866e7a959a50992bf0d4c1a5b
Nazarii <noreply1069@gmail.com>
2022-12-21 21:56:39 +0100

    UI logic

--- Assets/Scripts/UI/MenuManager.cs
+++ Assets/Scripts/UI/MenuManager.cs
@@ -2,9 +2,26 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UIElements;
 
 public class MenuManager : MonoBehaviour
 {
+    private Button playButton;
+    private Button optionsButton;
+    private Button quitButton;
+
+    private void Awake()
+    {
+        var root = GetComponent<UIDocument>().rootVisualElement;
+
+        playButton = root.Q<Button>("PlayButton");
+        optionsButton = root.Q<Button>("OptionsButton");
+        quitButton = root.Q<Button>("QuitButton");
+
+        playButton.clicked += PlayGame;
+        optionsButton.clicked += GoToSettings;
+        quitButton.clicked += QuitGame;
+    }
     public void PlayGame()
     {
         SceneManager.LoadScene("GameScene");
