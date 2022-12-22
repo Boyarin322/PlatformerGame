1ec27e70756dfdaec2e5122d8f4aed79292b9c69
Nazarii <noreply1069@gmail.com>
2022-12-22 20:40:19 +0100

    Added options

--- Assets/Scripts/UI/PauseManager.cs
+++ Assets/Scripts/UI/PauseManager.cs
@@ -3,11 +3,14 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UIElements;
 using UnityEngine.SceneManagement;
+using UnityEngine.Audio;
 
 public class PauseManager : MonoBehaviour
 {
     private bool gamePaused = false;
     [SerializeField] private GameObject pauseMenuUI;
+    [SerializeField] private GameObject optionsMenu;
+    [SerializeField] private AudioMixer globalAudioMixer;
     public void ResumeGame()
     {
         Time.timeScale = 1;
@@ -31,6 +34,20 @@ public class PauseManager : MonoBehaviour
     }
     public void GoToOptions()
     {
-
+        pauseMenuUI.SetActive(false);
+        optionsMenu.SetActive(true);
+    }
+    public void ExitOptions() 
+    {
+        pauseMenuUI.SetActive(true);
+        optionsMenu.SetActive(false);
+    }
+    public void SetFullscreen(bool isFullscreen)
+    {
+        Screen.fullScreen= isFullscreen;
+    }
+    public void SetVolume(float volume)
+    {
+        globalAudioMixer.SetFloat("volume", volume);
     }
 }
